877e7bb1eb3d2c0f62ccd338abfea0190f6fcbf7
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-04-25 19:52:39 +0530

    filter added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,6 +7,14 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Pagination from '@mui/material/Pagination';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
 import axios from "axios"
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router';
@@ -37,17 +45,120 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 
 export function Home() {
 
-  const navigate = useNavigate()
+  
+  const [age, setAge] = React.useState('');
   const [data,setData] = React.useState([])
+  const [page, setPage] = React.useState(1);
+
+  const [city ,setCity] = React.useState("")
+  const [verified ,setVerified ] = React.useState("")
+  const [cost,setCost] = React.useState("")
+  const [rating,setRating] = React.useState("")
+
   useEffect(()=>{getData()},[])
+  const navigate = useNavigate()
+
+  const handleChanges = (event, value) => {
+    setPage(value);
+  };
+
+
+  const handleChange1 = (event) => {
+    setCity(event.target.value);
+  };
+  const handleChange2 = (event) => {
+    setVerified(event.target.value);
+  };
+  const handleChange3 = (event) => {
+    setCost(event.target.value);
+  };
+  const handleChange4 = (event) => {
+    setRating(event.target.value);
+  };
+ 
 
   const getData = () => {
 
-    axios.get("http://localhost:8080/listing").then((res)=>{setData(res.data)})
+    axios.get(`http://localhost:8080/listing?page=${page}&city=${city}&verified=${verified}&cost=${cost}&rating=${rating}`).then((res)=>{setData(res.data)})
   }
 
-
+  
   return (
+    <>
+
+    {/* Filter part */}
+    
+    <div className="selectTag">
+
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">City</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={city}
+          label="City"
+          onChange={handleChange1}
+        >
+          <MenuItem value={"New Delhi"}>New Delhi</MenuItem>
+          <MenuItem value={"Mumbai"}>Mumbai</MenuItem>
+          <MenuItem value={"Hyderabad"}>Hyderabad</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Verified</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={verified}
+          label="Verified"
+          onChange={handleChange2}
+        >
+          <MenuItem value={"Yes"}>Yes</MenuItem>
+          <MenuItem value={"No"}>No</MenuItem>
+          
+        </Select>
+      </FormControl>
+    </Box>
+
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Cost Per Day</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={cost}
+          label="Cost Per Day"
+          onChange={handleChange3}
+        >
+          <MenuItem value={1}>High To Low</MenuItem>
+          <MenuItem value={-1}>Low To High</MenuItem>
+          
+        </Select>
+      </FormControl>
+    </Box>
+
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Rating</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={rating}
+          label="Rating"
+          onChange={handleChange4}
+        >
+          <MenuItem value={1}>High To Low</MenuItem>
+          <MenuItem value={-1}>Low To High</MenuItem>
+          
+        </Select>
+      </FormControl>
+    </Box>
+    </div>
+
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 700 }} aria-label="customized table">
         <TableHead>
@@ -81,5 +192,18 @@ export function Home() {
         </TableBody>
       </Table>
     </TableContainer>
-  );
+
+
+
+    {/* pagination */}
+
+    <Stack spacing={2}>
+      <Typography>Page: {page}</Typography>
+      <Pagination id="pagination" count={4} page={page} onChange={handleChanges} />
+    </Stack>
+
+
+
+    </>
+  )
 }
